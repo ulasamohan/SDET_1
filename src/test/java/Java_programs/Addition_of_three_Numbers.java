b2a8758d22b99d88620f8bc48d3a81aeb0eacb84
ulasamohan <ulasamohan1992@gmail.com>
2021-04-27 17:19:07 +0530

    new files added

---
+++ src/test/java/Java_programs/Addition_of_three_Numbers.java
@@ -0,0 +1,10 @@
+package Java_programs;
+
+public class Addition_of_three_Numbers {
+
+	public static void main(String[] args) {
+		System.out.println("this class is for addtion of three numbers");
+
+	}
+
+}
--- src/test/java/Java_programs/Addtion_of_two_Numbers.java
+++ src/test/java/Java_programs/Addtion_of_two_Numbers.java
@@ -3,7 +3,7 @@ package Java_programs;
 public class Addtion_of_two_Numbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
 
 	}
 
